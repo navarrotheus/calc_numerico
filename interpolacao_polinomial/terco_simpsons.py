797aff464f72a1fae37c28dc9419721ba46ce3ee
navarrotheus <matheusnavarro@alu.ufc.br>
2019-06-26 19:50:51 -0300

    add inputs

---
+++ interpolacao_polinomial/terco_simpsons.py
@@ -0,0 +1,7 @@
+g = int(input("Qual o grau do polinomio?"))
+for i in range(0,g+1) :
+    print("Entre o valor do x"+str(i))
+    xi = input()
+for i in range(0,g+1) :
+    print("Entre o valor do y"+str(i))
+    yi = input()
