8b3b58308bcede72eed82cd84ada87d30c6366ad
navarrotheus <matheusnavarro@alu.ufc.br>
2019-06-26 19:57:56 -0300

    otimizando input

--- interpolacao_polinomial/lagrange.py
+++ interpolacao_polinomial/lagrange.py
@@ -1,7 +1,5 @@
 g = int(input("Qual o grau do polinomio?"))
 for i in range(0,g+1) :
-    print("Entre o valor do x"+str(i))
-    xi = input()
+    xi = float(input("Entre o valor do x"+str(i)+":\n"))
 for i in range(0,g+1) :
-    print("Entre o valor do y"+str(i))
-    yi = input()
+    yi = float(input("Entre o valor do y"+str(i)+":\n"))
