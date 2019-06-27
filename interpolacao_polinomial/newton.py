c64c574df424139bc3017118563334198f4a9800
navarrotheus <matheusnavarro@alu.ufc.br>
2019-06-27 00:49:05 -0300

    funcao produtorio & somatorio

--- interpolacao_polinomial/newton.py
+++ interpolacao_polinomial/newton.py
@@ -19,10 +19,33 @@ n = g+1 #numero de pontos
 
 #funcao para encontrar as ordens d0,d1,...,dn
 def d(x,y,j,l):
+    #x,y coordenadas dos pontos
+    #j,l indices do respectivo ponto
     if j==0: #verifica se eh de ordem 0
         return y[0]
     elif j-l==1: #verifica se eh de ordem 1
         return (y[j]-y[l])/(x[j]-x[l])
     else: #ultimo caso ordem > 1
-        return (d(j,l+1,x,y)-d(j-1,l,x,y))/(x[j]-x[l])
+        return (d(x,y,j,l+1)-d(x,y,j-1,l))/(x[j]-x[l]) #recursividade ate chegar em ordem 1
+
+#funcao para o produtorio
+def p(x,j,z):
+    #x abscissas
+    #j indice da respectiva abscissa
+    #z valor interpolado
+    produtorio = 1
+    for i in range(j):
+        produtorio *= (z-x[i])
+
+#funcao para o somatorio
+def s(x,y,z,n):
+    #x,y coordenadas dos pontos
+    #z valor interpolado
+    #n numero de pontos
+    somatorio = 0
+    for j in range(n):
+        somatorio += d(j,0,x,y)*p(j,z,x)
+    return somatorio
+
+
 
