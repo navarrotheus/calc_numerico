14bb9a73c008d4320ba5cb494d0fa9a6bc2f9df3
navarrotheus <matheusnavarro@alu.ufc.br>
2019-06-26 22:44:20 -0300

    funcao lagrange

--- interpolacao_polinomial/lagrange.py
+++ interpolacao_polinomial/lagrange.py
@@ -1,9 +1,7 @@
-import numpy as np
-
 #entradas da tabela
-g = int(input("Qual o grau do polinomio?"))
-x = []
-y = []
+g = int(input("Qual o grau do polinomio?\n"))
+x = [] #coordenadas x's dos pontos
+y = [] #coordenadas y's dos pontos
 i = 0
 while len(x) < g+1:
     xi = float(input("Entre o valor do x"+str(i)+":\n"))
@@ -15,6 +13,8 @@ while len(y) < g+1:
     i += 1
     y.append(yi)
 
+xx = int(input("Entre o valor do dominio do polinomio a ser calculado:\n"))
+
 #interpolacao de lagrange
 def lagrange(xi,yi,x,g):
     #xi e yi sao as coordenadas dos pontos
@@ -23,16 +23,14 @@ def lagrange(xi,yi,x,g):
     somatorio = 0
     n = g+1 #numero de pontos
     for k in range(n):
-        def L(k) #calculo do fator de lagrange L
+        def L(k): #calculo do fator de lagrange L
             L = 1
             for j in range(n):
                 if k==j: #j!=k
                     continue
-                L *= float(x-x[j])/float(x[i]-x[j])
+                L *= float(x-xi[j])/float(xi[k]-xi[j]) #produtorio da formula
             return L
+        somatorio += yi[k]*L(k) #somatorio da formula
+    return somatorio
 
-
-
-     
-
-
+print(lagrange(x,y,xx,g))
