cf59d4b8a8e083f9467086a44e82078195919f7b
navarrotheus <matheusnavarro@alu.ufc.br>
2019-06-26 23:45:16 -0300

    separando funcoes lagrange

--- interpolacao_polinomial/lagrange.py
+++ interpolacao_polinomial/lagrange.py
@@ -13,24 +13,24 @@ while len(y) < g+1:
     i += 1
     y.append(yi)
 
-xx = int(input("Entre o valor do dominio do polinomio a ser calculado:\n"))
+xx = int(input("Entre o valor a ser interpolado:\n"))
 
 #interpolacao de lagrange
+def L(k,xi,x,n): #calculo do fator de lagrange L
+    L = 1
+    for j in range(n):
+        if k==j: #j!=k
+            continue
+        L *= float(x-xi[j])/float(xi[k]-xi[j]) #divisao dos produtorios da formula
+    return L
 def lagrange(xi,yi,x,g):
     #xi e yi sao as coordenadas dos pontos
-    #x eh o valor a ser calculado no polinomio
+    #x eh o valor a interpolar
     #g eh o grau do polinomio
     somatorio = 0
     n = g+1 #numero de pontos
     for k in range(n):
-        def L(k): #calculo do fator de lagrange L
-            L = 1
-            for j in range(n):
-                if k==j: #j!=k
-                    continue
-                L *= float(x-xi[j])/float(xi[k]-xi[j]) #produtorio da formula
-            return L
-        somatorio += yi[k]*L(k) #somatorio da formula
+        somatorio += yi[k]*L(k,xi,x,n) #somatorio da formula
     return somatorio
 
 print(lagrange(x,y,xx,g))
