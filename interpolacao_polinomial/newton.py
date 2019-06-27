403bf662d3663a6e5becbf3974f80bc5425bc685
navarrotheus <matheusnavarro@alu.ufc.br>
2019-06-27 01:21:20 -0300

    terminando interpolacao de newton

--- interpolacao_polinomial/newton.py
+++ interpolacao_polinomial/newton.py
@@ -1,51 +1,43 @@
 #entradas da tabela
-g = int(input("Qual o grau do polinomio?\n"))
-x = [] #coordenadas x's dos pontos
-y = [] #coordenadas y's dos pontos
+n = int(input("Qual o numero de pontos?\n"))
+x = [] #abscissas dos pontos
+y = [] #ordenadas dos pontos
 i = 0
-while len(x) < g+1:
+while len(x) < n:
     xi = float(input("Entre o valor do x"+str(i)+":\n"))
     i += 1
     x.append(xi)
 i = 0
-while len(y) < g+1:
+while len(y) < n:
     yi = float(input("Entre o valor do y"+str(i)+":\n"))
     i += 1
     y.append(yi)
 
 z = int(input("Entre o valor a ser interpolado:\n"))
 
-n = g+1 #numero de pontos
-
-#funcao para encontrar as ordens d0,d1,...,dn
+#funcao para encontrar a ordem d
 def d(x,y,j,l):
-    #x,y coordenadas dos pontos
-    #j,l indices do respectivo ponto
+    #x,y sao as coordenadas
+    #j,l sao os indices das coordenadas
     if j==0: #verifica se eh de ordem 0
-        return y[0]
+        return y[0] 
     elif j-l==1: #verifica se eh de ordem 1
-        return (y[j]-y[l])/(x[j]-x[l])
-    else: #ultimo caso ordem > 1
-        return (d(x,y,j,l+1)-d(x,y,j-1,l))/(x[j]-x[l]) #recursividade ate chegar em ordem 1
-
-#funcao para o produtorio
+        return (y[j]-y[l])/(x[j]-x[l]) 
+    else:
+        return (d(x,y,j,l+1)-d(x,y,j-1,l))/(x[j]-x[l]) #recursividade caso ordem > 1
+        
+#funcao para calcular o produtorio
 def p(x,j,z):
-    #x abscissas
-    #j indice da respectiva abscissa
-    #z valor interpolado
     produtorio = 1
     for i in range(j):
         produtorio *= (z-x[i])
+    return produtorio
 
-#funcao para o somatorio
+#funcao para calcular o somatorio
 def s(x,y,z,n):
-    #x,y coordenadas dos pontos
-    #z valor interpolado
-    #n numero de pontos
     somatorio = 0
     for j in range(n):
-        somatorio += d(j,0,x,y)*p(j,z,x)
+        somatorio += d(x,y,j,0)*p(x,j,z)
     return somatorio
 
-
-
+print(s(x,y,z,n))
