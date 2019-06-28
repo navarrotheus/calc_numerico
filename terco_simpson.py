38c60a604a64754b84e2f3eef9b899a57c86ef6b
navarrotheus <matheusnavarro@alu.ufc.br>
2019-06-28 10:05:54 -0300

    atualizando terco_simpson

--- terco_simpson.py
+++ terco_simpson.py
@@ -12,11 +12,11 @@ def terco_simpson(f,a,b,n):
     h=(b-a)/n #amplitude
     som1 = 0 #primeiro somatorio
     som2 = 0 #segundo somatorio
-    for i in range(1, int(n / 2 + 1)):
-        som1 += f(a + (2 * i - 1) * h)
-    for i in range(1, int(n / 2)):
-        som2 += f(a + (2 * i) * h)
-    integral = h/3*(f(a)+4*som1+2*som2+f(b)) #utilizando a formula
+    for i in range(1, int(n/2 + 1)):
+        som1 += f(a + (2*i - 1) * h)
+    for i in range(1, int(n/2)):
+        som2 += f(a + (2*i) * h)
+    integral = h/3*(f(a) + 4*som1 + 2*som2 + f(b)) #utilizando a formula
     return integral
 
 print(terco_simpson(f,a,b,n))
